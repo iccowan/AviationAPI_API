0872127acb2d1aa6eb4703270179ce1620999aa4
Ian Cowan <ian@cowanemail.com>
2019-04-02 18:04:59 -0400

    Working on fixing AFD

--- app/Console/Commands/PurgeChartDatabase.php
+++ app/Console/Commands/PurgeChartDatabase.php
@@ -47,9 +47,13 @@ class PurgeChartDatabase extends Command
     public function handle()
     {
         $next = DB::table('chart_update_cycles')->where('updated', 1)->first();
-        $now = Carbon::now();
-        $next_cycle = Carbon::create('20'.$next->year, $next->month, $next->day);
-        $time_until_next = $now->diffInDays($next_cycle);
+        if($next) {
+            $now = Carbon::now();
+            $next_cycle = Carbon::create('20'.$next->year, $next->month, $next->day);
+            $time_until_next = $now->diffInDays($next_cycle);
+        } else {
+            $time_until_next = 1
+        }
 
         if($time_until_next == 0) {
             DB::table('charts_current')->truncate();
@@ -100,9 +104,13 @@ class PurgeChartDatabase extends Command
         }
 
         $next = DB::table('afd_update_cycles')->where('updated', 1)->first();
-        $now = Carbon::now();
-        $next_cycle = Carbon::create('20'.$next->year, $next->month, $next->day);
-        $time_until_next = $now->diffInDays($next_cycle);
+        if($next) {
+            $now = Carbon::now();
+            $next_cycle = Carbon::create('20'.$next->year, $next->month, $next->day);
+            $time_until_next = $now->diffInDays($next_cycle);
+        } else {
+            $time_until_next = 1
+        }
 
         if($time_until_next = 0) {
             DB::table('afd_current')->truncate();
